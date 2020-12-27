5806933f6b358158c6acf978be6e9bed5c399aed
shota.sakanoue <s-mrchil07.7@ezweb.ne.jp>
2020-12-27 14:44:19 +0000

    Add Signup and Login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,58 @@
 class TasksController < ApplicationController
-    
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
-    
-    def index
-        @tasks = Task.all
-    end
-    
-    def show
-    end
-    
-    def new
-        @task = Task.new
-    end
-    
-    def create
-        @task = Task.new(task_params)
-        
-        if @task.save
-            flash[:success] = 'taskが正常に登録されました'
-            redirect_to @task
-        else
-            flash.now[:danger] = 'taskが登録されませんでした'
-            render :new
-        end
-    end
-    
-    def edit
-    end
-    
-    def update
-        if @task.update(task_params)
-            flash[:success] = 'taskが正常に更新されました'
-            redirect_to @task
-        else
-            flash.now[:danger] = 'taskは更新されませんでした'
-            render :edit
-        end
-    end
-    
-    def destroy
-        @task.destroy
-        
-        flash[:success] = 'taskは正常に削除されました'
-        redirect_to tasks_url
-    end
-    
-    private
-    
-    def set_task
-        @task = Task.find(params[:id])
-    end
-    
-    def task_params
-        params.require(:task).permit(:content, :status)
-    end
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
+  end
+  
+  def show
+  end
+  
+  def new
+      @task = Task.new
+  end
+  
+  def create
+      @task = Task.new(task_params)
+      
+      if @task.save
+          flash[:success] = 'taskが正常に登録されました'
+          redirect_to @task
+      else
+          flash.now[:danger] = 'taskが登録されませんでした'
+          render :new
+      end
+  end
+  
+  def edit
+  end
+  
+  def update
+      if @task.update(task_params)
+          flash[:success] = 'taskが正常に更新されました'
+          redirect_to @task
+      else
+          flash.now[:danger] = 'taskは更新されませんでした'
+          render :edit
+      end
+  end
+  
+  def destroy
+      @task.destroy
+      
+      flash[:success] = 'taskは正常に削除されました'
+      redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+      @task = Task.find(params[:id])
+  end
+  
+  def task_params
+      params.require(:task).permit(:content, :status)
+  end
 end
